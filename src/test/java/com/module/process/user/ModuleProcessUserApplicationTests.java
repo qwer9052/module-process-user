08350dc5f2d68f3b984e9036415dec5325770f09
qwer9052 <qwer9052@gmail.com>
2022-10-31 21:43:31 +0900

    [Add] 프로젝트 첫 커밋

---
+++ src/test/java/com/module/process/user/ModuleProcessUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.process.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleProcessUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
