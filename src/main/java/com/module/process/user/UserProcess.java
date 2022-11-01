926275f42f5c05462c120a7fbcb3a3188441faaa
qwer9052 <qwer9052@gmail.com>
2022-11-02 00:32:27 +0900

    [Add/Fix] git cache 초기화

--- src/main/java/com/module/process/user/UserProcess.java
+++ src/main/java/com/module/process/user/UserProcess.java
@@ -1,12 +1,13 @@
 package com.module.process.user;
 
+import com.module.domain.user.model.TbUserDto;
 import com.module.domain.user.rest.UserRest;
 import com.module.process.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserPrecess implements UserRest {
+public class UserProcess implements UserRest {
 
     @Autowired
     UserService userService;
@@ -17,5 +18,10 @@ public class UserPrecess implements UserRest {
         System.out.println("asdadasdad");
         userService.user();
 
+    }
+
+    @Override
+    public void createUser(TbUserDto tbUserDto) {
+        System.out.println("post user");
     }
 }
